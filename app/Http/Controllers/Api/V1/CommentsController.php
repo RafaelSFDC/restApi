28b2ddbff907cef63ee966d94fc7b2cb8d1a8062
Rafael <rafaelhibiki@gmail.com>
2024-02-09 16:27:26 -0300

    feat(Setup): :wrench: Controllers

---
+++ app/Http/Controllers/Api/V1/CommentsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comments;
+use App\Http\Requests\StoreCommentsRequest;
+use App\Http\Requests\UpdateCommentsRequest;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return Comments::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCommentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comments $comments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comments $comments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCommentsRequest $request, Comments $comments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comments $comments)
+    {
+        //
+    }
+}
